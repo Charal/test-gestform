6a2d6312e5766c4caf23a06c55359239e6fdcb4f
Alexandre Rault <alex.rault@laposte.net>
2022-11-30 09:37:18 +0100

    black formating

--- divided_by.py
+++ divided_by.py
@@ -5,28 +5,32 @@ import numpy as np
 def generate_random_integers(range_min=-1000, range_max=1000, amount=10):
     return list(np.random.randint(range_min, range_max, amount))
 
+
 # Is divisible by 3
 def is_geste(number):
     return number % 3 == 0
+
 
 # Is divisible by 5
 def is_forme(number):
     return number % 5 == 0
 
+
 def main():
     sentence = ""
     list_number = generate_random_integers()
 
-    for n in list_number :
+    for n in list_number:
         if is_geste(n) and is_forme(n):
             sentence = f"{n} >> Gestform"
         elif is_geste(n):
             sentence = f"{n} >> Geste"
         elif is_forme(n):
             sentence = f"{n} >> Forme"
-        else :
+        else:
             sentence = f"{n} >> {n}"
         print(sentence)
+
 
 if __name__ == "__main__":
     main()
--- tests/test_methods_unittest.py
+++ tests/test_methods_unittest.py
@@ -1,15 +1,16 @@
 import sys
+
 sys.path.append("../gestform")
 from divided_by import *
 import unittest
 
-class TestMethods(unittest.TestCase):
 
+class TestMethods(unittest.TestCase):
     def test_random_integers(self):
         number_list = generate_random_integers()
         self.assertIsInstance(number_list, list)
-        self.assertEqual(len(number_list),10)
-        for n in number_list :
+        self.assertEqual(len(number_list), 10)
+        for n in number_list:
             self.assertGreaterEqual(n, -1000)
             self.assertLessEqual(n, 1000)
 
@@ -31,5 +32,6 @@ class TestMethods(unittest.TestCase):
         self.assertFalse(is_forme(73))
         self.assertFalse(is_forme(5554))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
